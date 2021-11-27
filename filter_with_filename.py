506480ff54bcf908cb413486260213ff5e9a1023
yuliya <yulika20020604@gmail.com>
2021-11-27 10:25:34 +0500

    step 12

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -1,18 +1,4 @@
-from PIL import Image
-import numpy as np
-
-
-def convert_image_to_mosaic(image, size, gradation_step):
-    for x in range(0, len(image), size):
-        for y in range(0, len(image[0]), size):
-            image[x:x + size, y:y + size] = get_average_brightness(
-                image[x:x + size, y:y + size], size, gradation_step)
-    return image
-
-
-def get_average_brightness(block, size, gradation_step):
-    average_color = (block[:size, :size].sum() / 3) // size ** 2
-    return int(average_color // gradation_step) * gradation_step
+from filter import *
 
 
 def main():
